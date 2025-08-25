9c66618619c5b8639440932da6d82f6b9c1950df
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-08-25 10:22:09 -0600

    making my code work

--- Projects/who_are_you.py
+++ Projects/who_are_you.py
@@ -1,10 +1,9 @@
 # KH 2nd who_are_you project
 
 name=input("What's your name?")
-print(name)
 
 age=input( "What's you age?")
-print(age)
 
 fav_color=input("What's your favorite color?")
-print(fav_color)
+
+print(name + " " + age + " " + fav_color)
--- Starters/Seasons.py
+++ Starters/Seasons.py
@@ -2,4 +2,4 @@
 
 Name=input("Give me your last name")
 season=input("what's you favorite season")
-print ("Wow,name,"san,I like,"season,"too")
+print("Wow,name,"san,I like,"season,"too")
