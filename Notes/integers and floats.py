d804885d34a3a024e637ba3c860acd699e294875
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-08-28 10:51:30 -0600

    Average grade project

---
+++ Notes/integers and floats.py
@@ -0,0 +1,15 @@
+#2nd KH Integers and Floats notes
+
+#Integer examples
+age = 6549649999999999999999999   #Not a integer cause it's not a whole number
+temp = 85
+attention = -8
+spoons = 4        #it's a integer cause it's a whole number
+
+#Float examples (floating Point number)
+pi = 3.14
+pounds = 5.6
+emtional_damge = -.50
+
+#MATH
+print(8/4 +7 - (8+2)%4**2)
