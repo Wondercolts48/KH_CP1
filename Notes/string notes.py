16854d027fafa9da07e6f1bfe7cc354f47f3c80e
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-09-02 10:51:31 -0600

    String notes

--- Notes/string notes.py
+++ Notes/string notes.py
@@ -1,2 +1,31 @@
 #KH 2nd string notes
 
+#Examples:
+sentence = "The quick brown fox jumps over the lazy dog!" 
+first_name = 'Xavier'
+month = 'September'
+food = "chocolate"
+last_name = "LaRose"
+
+# If i need to use ' or "
+name = "La'Rose"
+best = '"The return o the King" is the best book ever!'
+
+#Length of strings
+
+length = len(month)
+print("september has", length, "letters")
+
+#Escape character
+print('Will said  "Don\'t die"')
+
+#Concatenate
+full_name = first_name + " "+  last_name
+
+print(full_name)
+
+#Index
+
+
+#Slice
+print(last_name[2:6])
