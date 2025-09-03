5f50b5ee95f5da0dcbf6b736bb02f4e1810fbfab
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-09-03 10:51:18 -0600

    madlib practice

---
+++ Practices/madlib.py
@@ -0,0 +1,9 @@
+# Kh 2nd Mablib
+
+animal = input("Give me a animal" " ")
+adjective = input("Give me an adjective" " ")
+noun = input("Now give me a noun" " ")
+food = input("Give me a type of food" " ")
+places = input("Give me your favorite place" " ")
+
+message = "If you\'ve ever met my" + animal, + " Snoopy, then you know he\'s not your average canine companion. Some kids might find it" + adjective + " that their beagle has such a" + adjective + "dog." + " Wht can\'t I have a" + noun + "just like everyone else? instead, I\'ve got a part-time pet, part-time World War I pilot, part time" + noun + "and a full time pain!" + adjective + "grief! Sometimes he looks at me like i\'m chopped" + food + " good for nothing except bringing his out to the" + places + "Still, it sure is nic eot have someone with really" + adjective + "ears around"
