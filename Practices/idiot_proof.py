efab9eb6ce6c1e095616d5bc44bc00e971f3e1b1
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-09-04 10:50:53 -0600

    idiot proof practice

--- Practices/idiot_proof.py
+++ Practices/idiot_proof.py
@@ -1,1 +1,8 @@
 # KH 2nd idiot proof practice
+
+first = ("Give your me first name".title()) 
+last =("Give me your last name")
+phone = ("please give me your phone number")
+gpa =("Now give me your GPA")
+
+print
