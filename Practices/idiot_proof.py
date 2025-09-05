2830ca3b07caeff7ed78585adb4cd552950b07a5
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-09-05 16:21:11 -0600

    finished code for project

--- Practices/idiot_proof.py
+++ Practices/idiot_proof.py
@@ -1,9 +1,11 @@
 # KH 2nd idiot proof practice
 
 full_name = input("Give your me full name." " ").title().strip()
-phone = input("please give me your phone number." " ")
-#gpa =input("Now give me your GPA." " ")
+phone = input("please give me your phone number using dashes please." " ")
+gpa = float(input("Now give me your GPA." " "))
 
-print(
+print(phone.replace("-"," "))
 
-print(f"Welcome" " " + full_name, + "Is your phone number?" + phone)
+print(round(gpa,1))
+
+print(f"Welcome" " ", full_name,", is your phone number?", phone.replace("-"," "),",this is also your GPA", (round(gpa,1)))
