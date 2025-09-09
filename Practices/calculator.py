26a28c3a6f8a01d030c800e6bd82456173dca802
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-09-09 10:44:13 -0600

    calculator finished

--- Practices/calculator.py
+++ Practices/calculator.py
@@ -5,10 +5,16 @@ two_num = float(input("Enter your second number" " "))
 
 add = one_num + two_num
 sub = one_num - two_num
-div = one_num / two_num
+division = one_num / two_num
+exp = one_num ** two_num
+multi = one_num * two_num
+modulo = one_num % two_num
+int_division = one_num // two_num
 
-
-
-
-
-print((one_num) + (two_num)) == (add)
+print(f"{one_num} + {two_num} = {add:.2f}")
+print(f"{one_num} - {two_num} = {sub:.2f}")
+print(f"{one_num} / {two_num} = {division:.2f}")
+print(f"{one_num} ** {two_num} = {exp:.2f}")
+print(f"{one_num} * {two_num} = {multi:.2f}")
+print(f"{one_num} % {two_num} = {modulo:.2f}")
+print(f"{one_num} // {two_num} = {int_division:.2f}")
