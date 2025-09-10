aae7dd81b6e37ea30b70a8be16bb82da1771136c
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-09-10 10:51:08 -0600

    crew project

---
+++ Projects/crew_shares.py
@@ -0,0 +1,6 @@
+# KH 2nd Crew Share projects
+
+money_earned = float(input("How much money have you earned?: "))
+crew_mem =int(input("How many crew memebers are there on the boat?: "))
+
+money_earned = money_earned - crew_mem
