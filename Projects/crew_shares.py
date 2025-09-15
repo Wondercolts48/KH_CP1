9075d48cda7f8a9a7fd560dbf23a09ae18faacfb
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-09-15 10:44:32 -0600

    crew shares project finsihed

--- Projects/crew_shares.py
+++ Projects/crew_shares.py
@@ -2,5 +2,15 @@
 
 money_earned = float(input("How much money have you earned?: "))
 crew_mem =int(input("How many crew memebers are there on the boat?: "))
+divvy = money_earned - (crew_mem * 500)
 
-money_earned = money_earned - crew_mem
+crew_mem_after = crew_mem - 2
+
+shares = divvy / crew_mem_after
+
+captain = shares * 7
+first_mate = shares * 3
+crew_mem = shares - 500
+print(f"The captain gets {captain:.2f}")
+print(f"The first mate gets {first_mate:.2f}")
+print(f"the crew members still needs {crew_mem:.2f}")
