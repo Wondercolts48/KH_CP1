b6c9d8a6914ccd95ba27f90254319a1b1882bd92
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-09-24 10:51:08 -0600

    grade project

---
+++ Practices/What_is_my_grade.py
@@ -0,0 +1,28 @@
+# Kh 2nd What is my grade project
+
+grade = float(input("What is your grade percentage? "))
+
+if grade >=97:
+    print("You have an A+!")
+elif grade >= 92:
+    print("You have an A")
+elif grade >= 90:
+    print("You have an A-")
+elif grade >= 87:
+    print("You have a B+")
+elif grade >= 83:
+    print("You have a B")
+elif grade >= 80:
+    print("You have a B-")
+elif grade >= 79:
+    print("You have a C+")
+elif grade >= 76:
+    print("You have a C")
+elif grade >= 70:
+    print("You have a C-")
+elif grade >= 69:
+    print("You have a D+")
+elif grade >= 65:
+    print("You have a D")
+else:
+    print("You have an F, do better")
