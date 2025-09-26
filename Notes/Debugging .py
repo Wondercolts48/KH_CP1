de2832a654b57f7fbc0f70c069e5395dd1121a4e
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-09-26 10:52:22 -0600

    trying to fix the game

---
+++ Notes/Debugging .py
@@ -0,0 +1,20 @@
+# Kh 2nd Bebugging Notes
+
+#Syntax Error - where you wrote the code wrong
+# print "MS.LaRose"
+# Fix by reading where the problem is AND fixing it!
+
+# Logic Error - The computer has the wrong steps
+num = "10"
+num_two = "3"
+
+print(num + num_two)
+
+
+# Run-Time Errors - The code woll start running and it will not finish
+import random
+
+while True:
+    denomenator = random.randint (0,5)
+
+    print(10/denomenator)
