530dc91d59bc3223fd710c5b21ae783e11d49800
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-09-30 10:51:31 -0600

    Shopping list program

---
+++ Practices/list_manager.py
@@ -0,0 +1,15 @@
+# Kh 2nd Shopping List Manager
+
+shopping_list = []
+while True:
+    action = input("What would you like to do to your list? Do you want to view, remove, or add to your list? ")
+    if action == "add":
+        new_item = input("What would you like to add to your list?")
+        shopping_list.append(new_item)
+        print(f"Here is your new shopping list {shopping_list}")
+    elif action == "remove":
+        remove_item = input("What would you like to remove from your list?")
+        shopping_list.remove(remove_item)
+        print("You removed", remove_item, "from your list, this is your new list now", shopping_list)
+    elif action == "exit":
+        break
