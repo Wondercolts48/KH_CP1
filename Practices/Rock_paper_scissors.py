f223c1b356b67453b7042a20dd7d593a761d593f
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-10-03 10:50:55 -0600

    Rock paper scissors practice

--- Practices/Rock_paper_scissors.py
+++ Practices/Rock_paper_scissors.py
@@ -1,2 +1,8 @@
 # Kh 2nd Rokc, paper, sissors practice
 
+
+game = input("Welcome to Rock, Paper, Scissors! Would you like to play today? ")
+if game == "yes":
+    print("then let the games begin!")
+
+elif game ==
