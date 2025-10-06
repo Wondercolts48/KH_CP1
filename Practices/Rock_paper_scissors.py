ef7a9c6b7e001cd12775dcc6355c9b4165092a43
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-10-06 10:51:43 -0600

    not finsihed code

--- Practices/Rock_paper_scissors.py
+++ Practices/Rock_paper_scissors.py
@@ -1,8 +1,24 @@
-# Kh 2nd Rokc, paper, sissors practice
+# Kh 2nd Rock, paper, scissors practice
 
+import random
+
+score = 0
+user_score = 0
+choices = ["rock", "paper", "scissors"]
 
 game = input("Welcome to Rock, Paper, Scissors! Would you like to play today? ")
 if game == "yes":
-    print("then let the games begin!")
+    print("then let's start the game!")
+    
 
-elif game ==
+while game:
+    user = ("rock", "paper", "scissors")
+    input("Choose, rock, paper, or scissors")
+    computer = random.choice(choices)
+
+if user == computer:
+    print("HA, it's a tie!")
+    user_score += 0
+elif (user == "paper" and computer == "scissors"):
+    print("I win! ")
+    user_score += 1
