8621fea0d10238b294d96415121b2367c64e5d7a
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-10-07 09:36:36 -0600

    Finished code, finally

--- Practices/Rock_paper_scissors.py
+++ Practices/Rock_paper_scissors.py
@@ -4,7 +4,7 @@ import random
 
 choices = ["rock", "paper", "scissors"]
 
-print("Welcome to Rock, Paper, Scissors!\nQuit: 'q'\nRock: 'r\nPaper: 'p")
+print("Welcome to Rock, Paper, Scissors!\nQuit: 'q'\nrock: 'r\npaper: 'p")
     
 
 while True:
@@ -16,15 +16,26 @@ while True:
                 
     elif player_choice == "rock"
         if computer == "rock":
-            print("tie")
+            print("It's a tie!")
         elif computer == "scissors":
             print("Ha, You won me.")
         elif computer == "paper":
             print("Ha, I won you.")
 
     elif player_choice == "paper"
+        if computer == "paper":
+            print("It's a tie!")
+        elif computer == "scissors":
+            print("Ha, I won!.")
+        elif computer == "paper":
+            print("Dang, you won.")
 
     elif player_choice == "scissors"
-
+        if computer == "scissors":
+            print("It's a tie!")
+        elif computer == "paper":
+            print("Ha, You won me.")
+        elif computer == "rock":
+            print("Ha, I won you.")
     else:
         print("Try again")
