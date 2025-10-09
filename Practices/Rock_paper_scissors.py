9f2a949de55aceb20e84d3d870a5414c0303df6b
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-10-09 10:21:09 -0600

    finished code with score this time

--- Practices/Rock_paper_scissors.py
+++ Practices/Rock_paper_scissors.py
@@ -3,7 +3,8 @@
 import random
 
 choices = ["rock", "paper", "scissors"]
-
+score_user = 0
+bot_score = 0
 print("Welcome to Rock, Paper, Scissors!\nQuit: 'q'\nrock: 'r\npaper: 'p")
     
 
@@ -12,6 +13,7 @@ while True:
     computer = random.choice(choices)
 
     if player_choice == "quit":
+        print("This is your score", score_user, "This is my score", bot_score)
         break
                 
     elif player_choice == "rock":
@@ -19,23 +21,31 @@ while True:
             print("It's a tie!")
         elif computer == "scissors":
             print("Ha, You won me.")
+            score_uesr = score_user + 1
         elif computer == "paper":
             print("Ha, I won you.")
+            bot_score = bot_score + 1
 
     elif player_choice == "paper":
         if computer == "paper":
             print("It's a tie!")
         elif computer == "scissors":
             print("Ha, I won!.")
+            bot_score = bot_score + 1
         elif computer == "paper":
             print("Dang, you won.")
+            score_user = score_user + 1
 
     elif player_choice == "scissors":
         if computer == "scissors":
             print("It's a tie!")
         elif computer == "paper":
             print("Ha, You won me.")
+            score_user = score_user + 1
         elif computer == "rock":
             print("Ha, I won you.")
-    else:
-        print("Try again")
+            bot_score = bot_score + 1
+
+        
+        else:
+            print("Try again")
--- Practices/tempCodeRunnerFile.py
+++ Practices/tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-
-        break
+"This is my score"
