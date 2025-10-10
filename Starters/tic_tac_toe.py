36f9131786c3093d2069c380841bf2979cce0f14
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-10-10 10:23:39 -0600

    Tic Tac toe group project

---
+++ Starters/tic_tac_toe.py
@@ -0,0 +1,33 @@
+# Kh 2nd tic tac Toe
+
+import random
+
+player = False
+computer = False
+
+board = [[1,2,3],
+         [4,5,6,],
+         [7,8,9]]
+
+if player == [1][2][3,]:
+        print("You won")
+
+elif player == [4][5][6]:
+        print("You won")
+
+elif player == [7][8][9]:
+        print("You won")
+
+elif player == [1][5][9]:
+        print("you won")
+elif player == [7][5][3]:
+        print("You won")
+
+elif player == [1][4][7]:
+        print("You won")
+
+elif player == [2][5][8]:
+    print("You won")
+
+elif player ==[3][6][9]:
+        print("You won")
