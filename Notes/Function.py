33cc1d515ef2e0bbd766ad59271ae13f4f19b5bf
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-10-20 10:51:34 -0600

    Function notes

---
+++ Notes/Function.py
@@ -0,0 +1,13 @@
+# KH 2nd Function notes
+
+def add(x, y):
+    return x + y
+
+while num < add(5, 5):
+    print("Duck")
+    num += 1
+print("Goose")
+print(f"Result is: {add(-45345436, 5434634)}")
+total = add(56456536, 65356354)
+print((add(add(3.14, .85),10)))
+add(42,7)
