318a447b2dd93790e505136bc15196ffb0ea953a
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-10-22 10:51:09 -0600

    Not a cesar cypher, doesnt work

--- Practices/Cesar_Cypher.py
+++ Practices/Cesar_Cypher.py
@@ -1,3 +1,17 @@
 # KH 2nd Cesar cypher encoder and decoder
+# Make a function to cypher up the message
+def encode():
+      # Ask what they want to do cypher
+    message = input("Hi, please put in your code you want to cypher ") 
+       #Make the whole code shift how many spaces the user wants
+    move = input("How many spaces do you want to shift? ")
+    # Have all my variables in a place where I can see
+    letter = ""
+    shift = int(shift)
 
-message = input("Hi, please put in your code you want to cypher")
+    for letter in message:
+        cypher = ord(letter) + shift
+        cypher = chr(cypher)
+    #then it prints out the code after it's already cyphered
+
+    print(message)
