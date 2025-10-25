1d293d3fc39d384c5a121ddb91f63dc061090e51
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-10-25 15:19:20 -0600

    Almost done code, keep going

--- Practices/combat.py
+++ Practices/combat.py
@@ -1,27 +1,42 @@
 # Kh 2nd Combat program
 
-
+import random   #See's if the user or the Monster will go first
 player = input("Welcome to training! First up, what is your name? ")
 print("Welcome", player)
 
 Class = input("Now choose your class of fighter, 1 is a fighter, 2 is a wizard, 3 is a spy. ")
 print("Great, here are your stats!")
 
-if Class == 1:
+if Class == str(1):
    
-   health = str(20)
-   print(str(health))
+   health = (20)
+   print(health)
    defense = 40
-   print(str(defense))
+   print(defense)
    damage = 15
-   print(str(damage))
+   print(damage)
 
-elif Class == 2:
+elif Class == str(2):
    health = 15
+   print(health)
    defense = 25
+   print(defense)
    damage = 30
+   print(damage)
 
-elif Class == 3:
+elif Class == str(3):
     health = 15
+    print(health)
     defense = 10
+    print(defense)
     damage = 10
+    print(damage)
+
+turn = random.choice(["user", "monster"])
+
+if turn == "user":
+   print("You will go first")
+else:
+   print("The monster goes first!")
+
+combat_method = input("Choose which combat ")
