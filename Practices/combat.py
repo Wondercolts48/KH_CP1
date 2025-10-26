909adf025c8acf140e2e1cb548bc68b3f83b956e
Wondercolts48 <kimberley.hernandez@ucas-edu.net>
2025-10-26 17:44:17 -0600

    Almost done code

--- Practices/combat.py
+++ Practices/combat.py
@@ -1,5 +1,6 @@
 # Kh 2nd Combat program
 
+import random
 
 player = input("Welcome to training! First up, what is your name? ")
 print("Welcome", player)
@@ -7,21 +8,41 @@ print("Welcome", player)
 Class = input("Now choose your class of fighter, 1 is a fighter, 2 is a wizard, 3 is a spy. ")
 print("Great, here are your stats!")
 
-if Class == 1:
+if Class == str(1):
    
-   health = str(20)
-   print(str(health))
+   health = (20)
+   print(health)
    defense = 40
-   print(str(defense))
+   print(defense)
    damage = 15
-   print(str(damage))
+   print(damage)
 
-elif Class == 2:
+elif Class == str(2):
    health = 15
+   print(health)
    defense = 25
+   print(defense)
    damage = 30
+   print(damage)
 
-elif Class == 3:
+elif Class == str(3):
     health = 15
+    print(health)
     defense = 10
+    print(defense)
     damage = 10
+    print(damage)
+
+   monster = 
+
+turn = random.choice(["user", "monster"])
+
+if turn == player:
+   print("You go first!")
+   print("Choose an action: ")
+   print("1. Attack")
+   print("2. defense")
+   print("3. Heal")
+
+if turn == 1
+player_hp
