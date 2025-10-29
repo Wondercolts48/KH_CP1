1b98865d788783902d48f97f6b451d3b43b27e6b
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-10-29 10:51:25 -0600

    Almost done

--- Practices/turtle_race.py
+++ Practices/turtle_race.py
@@ -4,15 +4,63 @@
 import turtle
 import random
 
+def speed():
+    t1.forward(random.randrange(1, 72))
+    t2.forward(random.randrange(1, 72))
+    t3.forward(random.randrange(1, 72))
+    t4.forward(random.randrange(1, 72))
+    t5.forward(random.randrange(1, 72))
+    
 
-turtle_1 = turtle.Turtle()
-turtle_2 = turtle.Turtle()
-turtle_3 = turtle.Turtle()
-turtle_4 = turtle.Turtle()
-turtle_5 = turtle.Turtle()
 
+
+#Have all my turtle's be a turtle
+t1 = turtle.Turtle()
+t2 = turtle.Turtle()
+t3 = turtle.Turtle()
+t4 = turtle.Turtle()
+t5 = turtle.Turtle()
+
+#Change my turtle colors
+t1.color("Light green")
+t1.shape("turtle")     #Make sure the variables are a turtle, like the animal
+t1.penup()
+t1.goto(-480,410)
+
+
+t2.color("Red")
+t2.shape("turtle")   #Make sure the variables are a turtle, like the animal
+t2.penup()
+t2.goto(-480,210)
+
+
+t3.color("Orange")
+t3.shape("turtle")   #Make sure the variables are a turtle, like the animal
+t3.penup()
+t3.goto(-480,10)
+
+
+t4.color("Light pink")
+t4.shape("turtle")   #Make sure the variables are a turtle, like the animal
+t4.penup()
+t4.goto(-480,-210)
+
+t5.color("Light blue")
+t5.shape("turtle")   #Make sure the variables are a turtle, like the animal
+t5.penup()
+t5.goto(-480,-410)
+
+#Making my finished line
+turtle.penup()
+turtle.goto(500,540)
+turtle.pensize(14)
+turtle.pendown()
 turtle.right(90)
-turtle.forward(150)
+turtle.forward(2000)
+
+#Making a While True
+while True:
+    speed()
 
 
 turtle.done()
