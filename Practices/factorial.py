cc9138bb94380360dcd0ecfc22e41c2eef48b781
Wondercolts48 <Kimberley.hernandez@ucas-edu.net>
2025-11-24 10:51:32 -0700

    Didn't do much

--- Practices/factorial.py
+++ Practices/factorial.py
@@ -1,19 +1,19 @@
 # Kh 2nd factorial calculator
 
 # Defining factorial calculator/calc function(userinput)
-def factorial(n)
 user_input = int(input("Enter a number: "))  #userinput
-result = 1
+def factorial(n):
+    result = 1
 #For x in range(user_input-1)
-for x in range( x, user_input - 1):
-    result = result * 1   # Added what happens when you get the result because my partner forgot
-return result
+    for x in range( x, user_input - 1):
+        result = result * 1   # Added what happens when you get the result because my partner forgot
+    return result
 
 #While True
 while True:
 #Use-choice is an integer: , (int method)
     if (user_input) == int:
-        print("")
+        print(" ")
         break     #Break
 
 #else: print/ displaying that the user needs to type an integer
